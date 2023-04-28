bb466b616e99584f3c19851a101948742c086aff
Hector Tanabe <hector.tanabe@jalasoft.com>
2023-04-28 19:22:37 -0400

    First-Class-Homework

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,13 +1,14 @@
 import pygame
 
 from pygame.sprite import Sprite
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 
 class Dinosaur(Sprite):
   X_POS = 80
   Y_POS = 310
   JUMP_SPEED = 8.5
+  Y_POS_DUCK = 340
   
   def __init__(self):
     self.image = RUNNING[0]
@@ -25,12 +26,19 @@ class Dinosaur(Sprite):
       self.run()
     elif self.dino_jump:
       self.jump()
+    elif self.dino_duck:
+      self.duck()    
       
     if user_input[pygame.K_UP] and not self.dino_jump:
       self.dino_jump = True
       self.dino_run = False
+    elif user_input[pygame.K_DOWN] and not self.dino_jump:
+      self.dino_jump = False
+      self.dino_run = False
+      self.dino_duck = True
     elif not self.dino_jump:
       self.dino_jump = False
+      self.dino_duck = False
       self.dino_run = True
       
     if self.step_index >= 10:
@@ -54,7 +62,11 @@ class Dinosaur(Sprite):
       self.dino_jump = False
       
   def duck(self):
-    pass
+    self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+    self.dino_rect = self.image.get_rect()
+    self.dino_rect.x = self.X_POS
+    self.dino_rect.y = self.Y_POS_DUCK
+    self.step_index += 1
   
   def draw(self, screen):
     screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
